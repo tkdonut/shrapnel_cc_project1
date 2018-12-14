7208bc660204718795002be17fa739d0c1ad52d5
Thomas Kendrick <tommkendrick@gmail.com>
2018-12-14 10:00:08 +0000

    initial commit

---
+++ db/shrapnel.sql
@@ -0,0 +1,25 @@
+DROP TABLE transactions;
+DROP TABLE tags;
+DROP TABLE vendors;
+
+
+CREATE TABLE tags
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE vendors
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE transactions
+(
+  id SERIAL8 PRIMARY KEY,
+  amount INT8,
+  tag_id INT8 references tags(id) ON DELETE SET NULL,
+  vendor_id INT8 references vendors(id) ON DELETE SET NULL
+);
+
