026e7993e5e190f756b607319049f97773945441
Thomas Kendrick <tommkendrick@gmail.com>
2018-12-14 11:55:15 +0000

    Added crud actions for tags

--- models/tag.rb
+++ models/tag.rb
@@ -1,7 +1,55 @@
 class Tag
+  attr_accessor :name
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
   end
-end
 
+# Class methods
+
+  def self.all()
+    sql = "SELECT * FROM tags"
+    results = SqlRunner.run( sql )
+    return results.map { |tag| Tag.new( tag ) }
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM tags"
+    SqlRunner.run( sql )
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM tags
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run( sql, values )
+    return Tag.new( results.first )
+  end
+
+# Instance methods
+
+  def save()
+    sql = 
+    "INSERT INTO tags (name)
+    VALUES ($1)
+    RETURNING id"
+    values = [@name]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+  def delete
+    sql = "DELETE FROM tags
+          WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def update
+    sql = 'UPDATE tags
+    SET name = $1
+    WHERE id = $2'
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+end
